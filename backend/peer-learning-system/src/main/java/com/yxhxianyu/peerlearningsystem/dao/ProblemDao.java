84bd5961713988daf74ddd612f8a9474cb86deaa
YXHXianYu <2943003@qq.com>
2023-10-07 23:59:57 +0800

    backend: fix dao mapper

--- backend/peer-learning-system/src/main/java/com/yxhxianyu/peerlearningsystem/dao/ProblemDao.java
+++ backend/peer-learning-system/src/main/java/com/yxhxianyu/peerlearningsystem/dao/ProblemDao.java
@@ -2,11 +2,13 @@ package com.yxhxianyu.peerlearningsystem.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.yxhxianyu.peerlearningsystem.pojo.ProblemPojo;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * DAO类，作为一个中间层，提供一个Java代码到数据库的映射
  * @author YXH_XianYu
  * @date 2023/10/7 23:24
  **/
+@Mapper
 public interface ProblemDao extends BaseMapper<ProblemPojo> {
 }
