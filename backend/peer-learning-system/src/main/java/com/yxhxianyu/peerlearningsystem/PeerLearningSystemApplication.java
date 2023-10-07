f40d9f8ccb9f1837eebe6b5d6d8a09eb8b978623
YXHXianYu <2943003@qq.com>
2023-10-07 21:02:02 +0800

    backend initial commit

---
+++ backend/peer-learning-system/src/main/java/com/yxhxianyu/peerlearningsystem/PeerLearningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yxhxianyu.peerlearningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerLearningSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerLearningSystemApplication.class, args);
+	}
+
+}
---
+++ backend/peer-learning-system/src/test/java/com/yxhxianyu/peerlearningsystem/PeerLearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxhxianyu.peerlearningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerLearningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
